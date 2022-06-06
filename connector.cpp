93ab809fda535dc6c39a18bc94eaae23f2b1ac65
Янбухтин Даниил <yanbuhtin_d@rusklimat.ru>
2022-06-06 11:07:36 +0000

    add minimal docs to connector.cpp

--- connector.cpp
+++ connector.cpp
@@ -5,19 +5,19 @@
 #include "commandHelper.h"
 using std::string;
 
-string getArgString(napi_env env, napi_callback_info info){
+string getArgString(napi_env env, napi_callback_info info){ //возвращает строку, переданную в js-функции в качестве аргумента. Да, это надо, тк напрямую получить данную строку нельзя
   size_t argc = 1;
   napi_value args[1];
   napi_get_cb_info(env, info, &argc, args, NULL, NULL);
-  char* choice;
+  char* choice; //напи, увы, не умеет просто так в std::string
   size_t str_size;
   napi_get_value_string_utf8(env, args[0], NULL, 0, &str_size);
   str_size++; //нуль-терминатор
   choice = (char*)calloc(str_size + 1, sizeof(char));
   size_t read;
   napi_get_value_string_utf8(env, args[0], choice, str_size, &read);
-  string final_str = string(choice);
-  delete [] choice;
+  string final_str = string(choice); 
+  delete [] choice; //чтобы память не текла
   return final_str;
 }
 
@@ -57,17 +57,17 @@ static napi_value removeLastCommand(napi_env env, napi_callback_info info){
   return result;
 }
 
-static napi_value run(napi_env env, napi_callback_info info){
+static napi_value run(napi_env env, napi_callback_info info){ //запускает записанную команду(не переданную в аргументе, а именно записанную ранее в command)
   string s = exec(command.c_str());
   std::cout << s << "\n";
   napi_value result;
   return result;
 }
-
+//дефайн для удобства
 #define DECLARE_NAPI_METHOD(name, func) \
   { name, 0, func, 0, 0, 0, napi_default, 0 }
 
-//а вот это, увы, отрефакторить никак нельзя
+
 static napi_value Init(napi_env env, napi_value exports) {
   napi_status status;
   napi_property_descriptor desc1 = DECLARE_NAPI_METHOD("chooseTestSet", chooseTestSet);
