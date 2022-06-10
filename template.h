a2aa1cf7b5c4ec9da190676acd6bde61918021b4
Янбухтин Даниил <yanbuhtin_d@rusklimat.ru>
2022-06-10 09:57:03 +0000

    many changes to template.h

--- template.h
+++ template.h
@@ -42,22 +42,70 @@ struct Template
         baseDir = dir;
         name = _name;
         chooseTestsWrappper(baseDir);
-        mkdir(name);
+        generateSelfDescribingFile();
     }
 
     Template(const string& enumedName, const string& _name){
         name = _name;
         chooseTests(enumedName);
         baseDir = chosen;
-        mkdir(name);
+        generateSelfDescribingFile();
     }
 
+    Template(const string& _name, const std::vector<string> &tests, const path& _baseDir) : name(_name), includedTests(tests), baseDir(_baseDir) {};
+
     void addExistingTest(const string& test){
-        // addCommand(string("cp " + test + " " + name)); Я не знаю, может ли это понадобится, но не хочу писать заново
-        // std::cout << command << "\n";
-        // system(command.c_str());
-        // removeLastCommand();
+        if (find(includedTests.begin(), includedTests.end(), test) != includedTests.end()){ //если тест уже есть - ничего не делаем
+            return;
+        }
         includedTests.push_back(test);
+        std::ifstream in("templates.txt");
+        bool isPrevLine = false, isFound = false;
+        string curLine;
+        std::vector<string> prevContent, nextContent;
+        while (getline(in, curLine)){ //разделяет файл на то, что должно быть до названия теста, и после него
+            if (isFound){
+                isPrevLine = false;
+                nextContent.push_back(curLine);
+            } else {
+                prevContent.push_back(curLine);
+            }
+            if (isPrevLine){
+                int tmp = curLine.find(';'); //ищем название последнего теста
+                if (tmp != string::npos){
+                    isFound = true;
+                }
+                if (!isFound){ //в этом шаблоне еще нет тестов
+                    if (curLine.find(':') != string::npos){
+                        isFound = true;
+                    } else {
+                        throw std::runtime_error("templates.txt has been corrupted"); //нет строки с тестами после имени файла, что-то пошло не так
+                    }
+                }
+            }
+            if (curLine[0] == 'n'){
+                string cur = curLine.substr(curLine.find(':') + 1); //находим имя
+                if (cur == name){
+                    isPrevLine = true; //названия тестов всегда идут после имени шаблона
+                }
+            }
+        }
+        in.close();
+        std::ofstream clear("templates.txt"); //удаляем все из файла
+        clear.close();
+        std::ofstream out("templates.txt");
+        for (int i = 0; i < (int)prevContent.size(); ++i){
+            if (i != (int)prevContent.size() - 1){
+                out << prevContent[i] << "\n";
+            } else {
+                out << prevContent[i];
+            }
+        }
+        out << test << ";\n";
+        for (const auto& i : nextContent){
+            out << i << "\n";
+        }
+        out.close();
     }
 
     void removeExistingTest(const string& test){
@@ -79,6 +127,19 @@ struct Template
         res = runTest(alltests);
         removeLastCommand();
         return res;
+    }
+
+    void generateSelfDescribingFile(){ //добавляет шаблон в файл templates.txt в виде name:<name>, includedTests:<test1; test2; ...>, baseDir:<baseDir>
+        std::ofstream out("templates.txt", std::ios_base::app);
+        out << "name:" << name << "\n";
+        out << "includedTests:";
+        for (int i = 0; i < (int)includedTests.size(); ++i){
+            out << includedTests[i] << ";";
+        }
+        out << "\n";
+        out << "baseDir:" << baseDir.string();
+        out << "\n";
+        out.close();
     }
 
 };
