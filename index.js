c4b6367b07d7105f5a709cbe0efc3467090ceb5e
Янбухтин Даниил <yanbuhtin_d@rusklimat.ru>
2022-06-10 06:54:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const addon = require('./build/Release/testaddon.node')
-
+const fs = require('fs')
 const TestSets = { B2C: 'b2c', B2B_SMOKE: 'b2b-smoke', B2B_REGRESS: 'b2b-regress'};
 
+function writeToFile(fileName, array){
+    var file = fs.createWriteStream(fileName);
+    array.forEach(function(v) { file.write(v + '\n'); });
+    file.end();
+}
+
 addon.chooseTestSet(TestSets.B2C);
+addon.createTemplate("templ1");
+addon.addExistingTest("templ1", "rejected.spec.ts");
+var arr = addon.runTestsInTemplate("templ1");
+writeToFile("out.rtf", arr);
+addon.deleteTemplate("templ1");
+
 
 console.log("ok");
